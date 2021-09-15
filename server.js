8870f836a966a54d2f227f3b6bc265d71376390b
Glitch (yielding-periwinkle-pajama) <none>
2021-09-15 22:08:20 +0000

    🍱🍂 Checkpoint ./server.js:17221034/196 ./watch.json:17221034/2

--- server.js
+++ server.js
@@ -126,11 +126,10 @@ async function getCourses(client, dbGroup) {
     // Check if we have previously stored a token.
     let token = await dbGroup.token;
     oAuth2Client.setCredentials(token);
-    console.log(oAuth2Client)
     const classroom = await google.classroom({ version: "v1", auth: oAuth2Client });
-    console.log(classroom)
-    const course = await classroom.courses.get(dbGroup.course);
-    console.log(typeof(course))
+    const course = await classroom.courses.get({id: dbGroup.course});
+    console.log(course)
+    console.log(await course.courseWork.get())
   });
 }
 async function getNewToken(oAuth2Client, callback, message, client) {
