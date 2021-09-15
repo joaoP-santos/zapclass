bac2e22fa3db5d5f9d54bc3601c34b5a0a204154
Glitch (yielding-periwinkle-pajama) <none>
2021-09-15 23:05:41 +0000

    🔖🚓 Checkpoint ./server.js:17221034/103

--- server.js
+++ server.js
@@ -128,7 +128,8 @@ async function getCourses(client, dbGroup) {
     oAuth2Client.setCredentials(token);
     const classroom = await google.classroom({ version: "v1", auth: oAuth2Client });
     const course = await classroom.courses.get({id: dbGroup.course});
-    console.log(course)
+    const courseworks = await classroom.courses.courseWork.list(course)
+    
     console.log(await course.courseWork.get())
   });
 }
