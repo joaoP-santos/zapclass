ed7888a2f67b30add9bc159b55d0da405ba303ed
Glitch (yielding-periwinkle-pajama) <none>
2021-09-16 14:59:02 +0000

    ❤️🌇 Checkpoint ./server.js:17221034/34480 ./watch.json:17221034/4

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const SCOPES = [
   "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
   "https://www.googleapis.com/auth/classroom.courses.readonly",
   "https://www.googleapis.com/auth/classroom.courses",
-  "https://www.googleapis.com/auth/classroom.profile.emails"
+  
+    "https://www.googleapis.com/auth/classroom.rosters",
+    "https://www.googleapis.com/auth/classroom.rosters.readonly",
+    "https://www.googleapis.com/auth/classroom.profile.emails",
+    "https://www.googleapis.com/auth/classroom.profile.photos",
+
 ];
 
 const TOKEN_PATH = "token.json";
